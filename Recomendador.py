1225d97766cd9c56df4ddcab1ba6b25cbb241fcb
Bruno Vieira Santos <163612337+bruunovsanttos@users.noreply.github.com>
2024-07-25 12:51:41 -0300

    Add files via upload
    Adição de filmes, livros e games nos dicionários

--- Recomendador.py
+++ Recomendador.py
@@ -95,12 +95,47 @@ def recomendar():
             print(f"Alguma coisa deu errado {nome_usuario}, vamos tentar novamente?")
 
 
-
     
 
-filmes = {}
-games = {}
-livros = {}
+filmes = {
+    "Star Wars" ["1977", "George Lucas"]
+    "O Guia Do Mochileiro das Galáxias" ["2005", "Garth Jennings"]
+    "De Volta Para o Futuro" ["1985", "Robert Zemeckis"]
+    "Duna" ["2021", "Denis Villeneuve"]
+    "Jogador Nº 1" ["2018", "Steven Spielberg"]
+    "Os Caça Fantasmas" ["1984", "Ivan Reitman"]
+    "Matrix" ["1999", "Lana Wachowski, Lilly Wachowski"]
+    "Akira" ["1991", "Katsuhiro Otomo"]
+    "Interestrelar" ["2014", "Christopher Nolan"]
+    "Blade Runner" ["1982", "Ridley Scott"]
+    "Senhor dos Anéis" ["2001" "Peter Jackson"]
+}
+
+games = {
+    "Mario Bros." ["1983", "Shigeru Miyamoto, Gunpei Yokoi"]
+    "Sonic the Hedgehog" ["1991", "Yuji Naka"]
+    "The Legend of Zelda: Ocarina of Time" ["1998", "Shigeru Miyamoto, Koji Kondo, Eiji Aonuma, Yoshiaki Koizumi, Takashi Tezuka, Yoichi Yamada"]
+    "Metroid" ["1986", "Gunpei Yokoi"]
+    "Metal Slug" ["1996", "Nazca Corporation, SNK, Irem"]
+    "Metal Gear" ["1987", "Hideo Kojima"]
+    "Grand Theft Auto (GTA)" ["1997", "Rockstar North, Rockstar Lincoln, Visual Science, DMA Design"]
+    "Tetris" ["1984", "Alexey Pajitnov, Sega, Spectrum Holobyte"]
+    "Pac-Man" ["1980", "Toru Iwatani"]
+    "Pokemon" ["1996", "Ken Sugimori, Satoshi Tajiri"]
+}
+
+livros = {
+    "O Guai do Mochieliro Das Galáxias" ["1979", "Douglas Adams"]
+    "O Senhor Dos Anéis" ["1954", "J. R. R. Tolkien"]
+    "Duna" ["1965", "Frank Herbert"]
+    "2001 - Uma Odisseia no espaço" ["1968", "Arthur C. Clarke"]
+    "Jogos Vorazes" ["2008", "Suzanne Collins"]
+    "Percy Jackson" ["2005", "Rick Riordan"]
+    "Harry Potter" ["1997", "J. K. Rowling"]
+    "Viagem ao Centro Da Terra" ["1864", "Júlio Verne"]
+    "As Crônicas de Gelo e do Fogo" ["1996", "George R.R. Martin"]
+    "Neuromancer" ["1984", "William Gibson"]
+}
 
 while True:
     
